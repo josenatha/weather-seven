e0a11a1a378933e967cf0227f3218d6c8d9241c5
natha <josenathanielc@gmail.com>
2022-04-14 14:16:02 -0500

    gear secando

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -18,7 +18,7 @@ export const WeatherCard = ({ city, country,weather,temp,icon}) => {
         <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z"/>
         </svg></span>
         {temperature? temperatureC.toFixed(2) + ' °C ':temperatureF.toFixed(2) + ' °F '} </p>
-        <img src={` http://openweathermap.org/img/wn/${icon}@2x.png`} />
+        <img  alt='icon' src={` http://openweathermap.org/img/wn/${icon}@2x.png`} />
         <button onClick={() => setTemperature(!temperature)}>
         {temperature ? " Convert to °F " : "Convert to °C "}
        
